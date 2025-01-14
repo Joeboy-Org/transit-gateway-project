aa5bad5c88a8137cb4bd78c545db130481afffb6
joseph okorie <tp-ver-jo@ramsayhealth.com.au>
2025-01-15 09:20:16 +1000

    another commit

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "caller_identity_application" {
-  value = data.aws_caller_identity.application[0].account_id
-}
+# output "caller_identity_application" {
+#   value = data.aws_caller_identity.application[0].account_id
+# }
