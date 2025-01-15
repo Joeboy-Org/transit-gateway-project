3ea981e2a82cb201f2f1bd254d77939906f3a9b6
joseph okorie <tp-ver-jo@ramsayhealth.com.au>
2025-01-15 16:11:48 +1000

    initial commit

--- modules/transit-gw/data.tf
+++ modules/transit-gw/data.tf
@@ -1,5 +1,5 @@
 data "aws_ec2_transit_gateway_attachment" "devops_application" {
-  transit_gateway_attachment_id = "tgw-attach-068e0c18c1de02ca3"
+  transit_gateway_attachment_id = "tgw-attach-01c6d01136f33167f"
   filter {
     name   = "transit-gateway-id"
     values = [aws_ec2_transit_gateway.this[0].id]
