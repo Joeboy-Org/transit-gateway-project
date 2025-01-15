72e1b61d308b445e2a3cb8a34964ef4fa8285204
joseph okorie <tp-ver-jo@ramsayhealth.com.au>
2025-01-15 13:33:49 +1000

    initial commit

---
+++ locals.tf
@@ -0,0 +1,21 @@
+locals {
+  transit_public_subnets = {
+    public-subnet-A = {
+      availability_zone = "ap-southeast-2a"
+      cidr_block        = "10.16.2.0/24"
+    }
+  }
+  transit_private_subnets = {
+    private-subnet-A = {
+      availability_zone  = "ap-southeast-2a"
+      cidr_block         = "10.16.1.0/24"
+      natgw_pub_sub_name = "public-subnet-A"
+    }
+  }
+  app_private_subnets = {
+    private-subnet-A = {
+      availability_zone = "ap-southeast-2a"
+      cidr_block        = "10.17.1.0/24"
+    }
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -1,35 +1,40 @@
 module "transit_vpc" {
-  count          = var.environment == "networking" ? 1 : 0
-  source         = "./modules/vpc"
-  vpc_name       = "transit"
-  vpc_cidr_block = "10.16.0.0/16"
-  private_subnets = {
-    private-subnet-A = {
-      availability_zone  = "ap-southeast-2a"
-      cidr_block         = "10.16.1.0/24"
-      natgw_pub_sub_name = "public-subnet-A"
+  count           = var.environment == "networking" ? 1 : 0
+  source          = "./modules/vpc"
+  vpc_name        = "transit"
+  vpc_cidr_block  = "10.16.0.0/16"
+  private_subnets = local.transit_private_subnets
+  public_subnets  = local.transit_public_subnets
+  tgw_vpc_public_routes = {
+    app_vpc_routes = {
+      cidr_block         = "10.17.0.0/16"
+      public_subnet_keys = keys(local.transit_public_subnets)
     }
   }
-  public_subnets = {
-    public-subnet-A = {
-      availability_zone = "ap-southeast-2a"
-      cidr_block        = "10.16.2.0/24"
+  tgw_vpc_private_routes = {
+    app_vpc_routes = {
+      cidr_block          = "10.17.0.0/16"
+      private_subnet_keys = keys(local.transit_private_subnets)
     }
   }
-  environment = var.environment
+  transit_gateway_id = module.transit-tgw-attachment[0].transit_gw_id
+  environment        = var.environment
 }
 
 module "application_vpc" {
-  count          = var.environment == "application" ? 1 : 0
-  source         = "./modules/vpc"
-  vpc_name       = "devops"
-  vpc_cidr_block = "10.17.0.0/16"
-  private_subnets = {
-    private-subnet-A = {
-      availability_zone = "ap-southeast-2a"
-      cidr_block        = "10.17.1.0/24"
+  count                 = var.environment == "application" ? 1 : 0
+  source                = "./modules/vpc"
+  vpc_name              = "devops"
+  vpc_cidr_block        = "10.17.0.0/16"
+  private_subnets       = local.app_private_subnets
+  public_subnets        = {}
+  tgw_vpc_public_routes = {}
+  tgw_vpc_private_routes = {
+    app_vpc_routes = {
+      cidr_block          = "0.0.0.0/0"
+      private_subnet_keys = keys(local.app_private_subnets)
     }
   }
-  public_subnets = {}
-  environment    = var.environment
+  transit_gateway_id = data.aws_ec2_transit_gateway.this[0].id
+  environment        = var.environment
 }
