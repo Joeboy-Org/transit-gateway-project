6333d6c7dbe6b0344c243551a43369a38d64e2f8
joseph okorie <tp-ver-jo@ramsayhealth.com.au>
2025-01-16 00:00:41 +1000

    new commit

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this" {
   ami                         = "ami-0078b36c740288eef"
   associate_public_ip_address = false
   instance_type               = "t3.medium"
-  vpc_security_group_ids      = [var.environment == "networking" ? aws_security_group.transit_sg.id : aws_security_group.devops_application_sg.id]
+  vpc_security_group_ids      = [var.environment == "networking" ? aws_security_group.transit_sg[0].id : aws_security_group.devops_application_sg[0].id]
   iam_instance_profile        = aws_iam_instance_profile.ssm_role[0].name
   subnet_id                   = var.environment == "networking" ? module.transit_vpc[0].private_subnet_id["private-subnet-A"].id : module.application_vpc[0].private_subnet_id["private-subnet-A"].id
   tags = {
